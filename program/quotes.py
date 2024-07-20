66e6308e80d980d1069e71d8bac001e395b2051c
CPU Governor <cpu.governor@gmail.com>
2024-07-20 12:35:35 +0100

    fix bugs

--- program/quotes.py
+++ program/quotes.py
@@ -1,7 +1,7 @@
 import random
 
 # Step 1: Create an empty list with a fixed size (if you know the number of quotes beforehand)
-quotes = [None] * 12  # Assuming we have 7 quotes
+quotes = [None] * 43  # Assuming we have 7 quotes
 
 # Step 2: Manually assign quotes to specific indices
 quotes[0] = "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela"
@@ -16,6 +16,37 @@ quotes[8] = "Whether you think you can, or you think you can't; you're right -He
 quotes[9] = "You can, you should, and if you’re brave enough to start, you will - Stephen King"
 quotes[10] = "If I had asked people what they wanted, they would have said faster horses -Henry Ford"
 quotes[11] = "Enjoy the pain (a key to not giving up)"
+quotes[12] = "Discover your purpose, and let it consume you."
+quotes[13] = "Discipline is choosing between what you want now and what you want most. - Abraham Lincoln"
+quotes[14] = "Yeah i know that they are better than you, but all i ask is that you just be persistent. they will make a mistake that will benefit you."
+quotes[15] = "Only those who will risk going too far can possibly find out how far one can go - T.S. Eliot"
+quotes[16] = "Disapear, come back and shack the world"
+quotes[17] = "What should we call our selves now? Failure or Success waiting to happen? Bruh when are you happening?"
+quotes[18] = "Suffering doesn't mean you will be successful ... you might even Die..!!"
+quotes[19] = "Do not worry about anything; instead pray about everything. - PHILIPPIANS 4:6"
+quotes[20] = "Today i will be in my room like an inmate; i must get this job done!!"
+quotes[21] = "Greatness is a journey, not a destination."
+quotes[22] = "Don't hesitate and Don't work with people that procrastinate.."
+quotes[23] = "Regret for time wasted is wasting more time"
+quotes[24] = "Your days are better when you focus on your blessings more than your problems."
+quotes[25] = "Not grinding to get paid, i'm grinding to stay paid."
+quotes[26] = "Not everyone likes me, but not everyone matters."
+quotes[27] = "There are Known Knowns and there are Known unKnowns But there are also unknown unKnowns; things we don't know that we don't know -Samuel L jackson"
+quotes[28] = "Don't confuse SCHOOLING with EDUCATION. i didn't go to harvard, but the people who worked for me did - Elon musk"
+quotes[29] = "I love walking in the rain cause no one can see me crying - Charlie chapplin"
+quotes[30] = "In the game of life, death always wins. All we can do is choose how to play"
+quotes[31] = "If i tell you my dreams, you will think i am crazy"
+quotes[32] = "Great men are forged in fire. It is the privilege of lesser men to light the flame. - The War Doctor"
+quotes[33] = "Very private people have mastered the art of telling you little about themselves, but doing it in such a way that you think you know a lot -Uknown"
+quotes[34] = "They call us dreamers, but we're the ones who don't sleep."
+quotes[35] = "They call us dreamers, but we're the ones who don't sleep."
+quotes[36] = "Don't leave tomorrow's problems to tomorrow's you."
+quotes[37] = "Everything no be hardwork but dem no go tell you. -B.O.D"
+quotes[38] = "As a man you can't stop spending, just pray you don't stop earning"
+quotes[39] = "A dude messaged me on Linkedin 4years ago asking for an internship at my job, today i just messaged him applying to his company. you really never know."
+quotes[40] = "Push yourself. No one else is going to do it for you."
+quotes[41] = "if the opportunity doesn't exist, create it."
+quotes[42] = "if we wait until we're ready, we'll be waiting for the rest of our lives."
 
 # Step 3: Select a random quote
 random_quote = random.choice(quotes)
